d4c01676a39f61d7be811cafa85e4fe3626bd851
Pedro Almodovar <pedro.pinto@contabilizei.com.br>
2022-08-13 17:48:54 -0300

    feature: Iniciando com States

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,27 +1,28 @@
+import { useState } from "react";
 import Item from "./Item";
 import style from './Lista.module.scss'
 
 function Lista() {
-    let tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    },
-    {
-        tarefa: 'JavaScript',
-        tempo: "01:00:00"
-    },
-    {
-        tarefa: 'Typescript',
-        tempo: "03:00:00"
-    }
-    ]
+    const [tarefas, setTarefas] =  useState(
+        [{
+            tarefa: 'React',
+            tempo: '02:00:00'
+        },
+        {
+            tarefa: 'JavaScript',
+            tempo: "01:00:00"
+        },
+        {
+            tarefa: 'Typescript',
+            tempo: "03:00:00"
+        }]
+    );
 
 
     return (
         <aside className={style.listaTarefas}>
             <h2 onClick={() => {
-                console.log("h2 clicado")
-                tarefas = [...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}]
+                setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}]);
             }}> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
