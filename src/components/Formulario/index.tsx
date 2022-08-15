7aea665f2cef7e2ab622da210bac00eb7015499d
Pedro Almodovar <pedro.pinto@contabilizei.com.br>
2022-08-15 17:50:59 -0300

    adicionando clique em lista

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,51 +1,62 @@
 import React, { ReactComponentElement } from "react";
-import { convertTypeAcquisitionFromJson } from "typescript";
+import { convertTypeAcquisitionFromJson, textChangeRangeIsUnchanged } from "typescript";
 import Botao from "../Botao";
 import style from './Formulario.module.scss'
 import ITarefa from "../../types/tarefa";
+import { v4 as uuidv4 } from 'uuid';
 
-class Formulario extends React.Component <{
+class Formulario extends React.Component<{
     setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
 }>{
     state = {
-        tarefa : "",
-        tempo : "00:00:00"
-    } 
+        tarefa: "",
+        tempo: "00:00:00"
+    }
 
-    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
         evento.preventDefault();
-        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state}])
+        this.props.setTarefas(tarefasAntigas =>
+            [...tarefasAntigas,
+            { ...this.state, 
+                selecionado: false, 
+                completado: false,
+                id : uuidv4()    
+            }])
+        this.setState({
+            tarefa: "",
+            tempo: "00:00"
+        })
     }
 
     render() {
         return (
-            <form className={style.novaTarefa} onSubmit = {this.adicionarTarefa.bind(this)}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
-                   <label htmlFor="tarefa">
+                    <label htmlFor="tarefa">
                         Adicione um novo estudo
-                    </label> 
-                    <input type="text" 
-                    name="tarefa" 
-                    id="tarefa"
-                    placeholder="O que você quer estudar"
-                    value={this.state.tarefa}
-                    onChange = {evento => this.setState({...this.setState, tarefa : evento.target.value})}
-                    required
+                    </label>
+                    <input type="text"
+                        name="tarefa"
+                        id="tarefa"
+                        placeholder="O que você quer estudar"
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({ ...this.setState, tarefa: evento.target.value })}
+                        required
                     />
                 </div>
                 <div className={style.inputContainer}>
-                   <label htmlFor="tempo">
+                    <label htmlFor="tempo">
                         Tempo
-                    </label> 
-                    <input type="time" 
-                    step={"1"}
-                    name = "tempo"
-                    id = "tempo"
-                    min = "00:00:00"
-                    max = "01:30:00"
-                    value = {this.state.tempo}
-                    onChange = {evento => this.setState({...this.setState, tempo : evento.target.value})}
-                    required
+                    </label>
+                    <input type="time"
+                        step={"1"}
+                        name="tempo"
+                        id="tempo"
+                        min="00:00:00"
+                        max="01:30:00"
+                        value={this.state.tempo}
+                        onChange={evento => this.setState({ ...this.setState, tempo: evento.target.value })}
+                        required
                     />
                 </div>
                 <Botao
--- src/types/tarefa.ts
+++ src/types/tarefa.ts
@@ -1,4 +1,9 @@
+
+
 export default interface ITarefa {
     tarefa: string,
     tempo: string
+    selecionado: boolean,
+    completado: boolean,
+    id : string
 }
